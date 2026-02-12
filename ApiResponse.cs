fc76d6ca66e076c08500c9991b6381ded5806038
Houston-Miller <houston.miller@eltoro.com>
2026-02-12 14:18:46 -0500

    added new data handling and a new response to meet requirements

--- ApiResponse.cs
+++ ApiResponse.cs
@@ -1,4 +1,32 @@
-public record DailyData(string[] time, float[] temperature_2m_max, float[] precipitation_probability_mean);
+public record DailyData(string[] time, 
+    float[] temperature_2m_max, 
+    float[] precipitation_probability_mean, 
+    WeatherType[] weather_code);
 public record WeatherResponse(DailyData daily);
 public record LocationData(string name, float latitude, float longitude);
 public record GeoResponse(LocationData[] results);
+public enum WeatherType
+{
+    ClearSky = 0,
+    MainlyClear = 1,
+    PartlyCloudy = 2,
+    Overcast = 3,
+    
+    Fog = 45,
+    DepositingRimeFog = 48,
+    
+    DrizzleLight = 51,
+    DrizzleModerate = 53,
+    DrizzleDense = 55,
+    
+    RainSlight = 61,
+    RainModerate = 63,
+    RainHeavy = 65,
+    
+    SnowSlight = 71,
+    SnowModerate = 73,
+    SnowHeavy = 75,
+    
+    Thunderstorm = 95,
+    ThunderstormHeavy = 99
+}
